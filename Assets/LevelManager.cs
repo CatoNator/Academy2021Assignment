0508b0221335b7c0ab640961fb7cf10bc203983e
CatoNator <jesse.j.koivisto@hotmail.com>
2021-08-21 20:39:36 +0300

    Early spawning system

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class LevelManager : MonoBehaviour
 {
     //The player entity (in the scene)
-    public GameObject Player;
+    public GameObject PlayerEntity;
+
+    //The player entity (in the scene)
+    public Transform ObjectSpawnerTransform;
 
     //camera entity (in the scene)
-    public GameObject Camera;
+    public Transform CameraEntity;
 
     //This is for the UI element
     public GameObject TextRendererEntity;
@@ -18,6 +21,11 @@ public class LevelManager : MonoBehaviour
 
     //array of obstacle entity prefabs
     public GameObject[] ObstaclePrefabs;
+
+    //distance between two spawned objects
+    public float SpawnDistance = 0;
+
+    float LastSpawnPos = 0;
 
     //RNG, using the C# System library's implementation for simpler integer randomization
     System.Random RandomGenerator = new System.Random();
@@ -49,12 +57,52 @@ public class LevelManager : MonoBehaviour
     void Update()
     {
         //Check camera position in level, spawn new obstacles, stars or colour switchers if necessary
+        if (CameraEntity != null)
+        {
+            //
+            if (CameraEntity.position.y >= LastSpawnPos + SpawnDistance)
+            {
+                SpawnObject();
+                LastSpawnPos = CameraEntity.position.y;
+            }
+        }
+        else
+            Debug.LogWarning("LevelManager: Camera entity is null!");
 
         //stars and colour switchers have the same spawn behaviour
 
         //Updating the UI mesh
         if (TextMesh != null)
             TextMesh.text = StarCount.ToString();
+    }
+
+    private void SpawnObject()
+    {
+        //Random int to pick something to spawn
+        int ChooseObjectType = RandomGenerator.Next(0, 4);
+
+        //0 spawns a collectible - star or a colour switcher gizmo
+        if (ChooseObjectType == 0)
+        {
+            Debug.Log("Spawning collectible");
+
+            //spawn random collectible
+            ChooseObjectType = RandomGenerator.Next(0, CollectiblePrefabs.Length);
+
+            UnityEngine.Object.Instantiate(CollectiblePrefabs[ChooseObjectType], ObjectSpawnerTransform.position, Quaternion.identity);
+
+        }
+        else //otherwise we spawn an obstacle
+        {
+            Debug.Log("Spawning obstacle");
+
+            //spawn random obstacle
+            ChooseObjectType = RandomGenerator.Next(0, ObstaclePrefabs.Length);
+            GameObject ObstacleType = ObstaclePrefabs[ChooseObjectType];
+
+            //the reason for the addition is that some of the prefabs (like the blade) have offset, not including it makes the game basically impossible
+            UnityEngine.Object.Instantiate(ObstacleType, ObjectSpawnerTransform.position + ObstacleType.transform.position, Quaternion.identity);
+        }
     }
 
     public void AddStar()
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Academy2021Assignment
 
 ## Improvements to be done
-* I admittedly did not think very far when writing the initial code, and a flappy bird system where the game world moves areounf the player would've probably been a better choice for the spawning system.
+* The spawning algorithm is kind of lame... I did in quite a hurry, but I think it could be quite easily improved and made a lot more fun.
 
 ## Known bugs and other miscellany
 Some of the game's functions were designed around the limited scope, but could cause issues in the future:
@@ -9,3 +9,4 @@ Some of the game's functions were designed around the limited scope, but could c
 * The player's position on screen is tracked using a collider around the camera view, that kills the player upon exiting the volume. If the player happens to get above the camera, the collider will also kill the player. This doesn't happen within this prototype, but could happen if someone was to tweak the camera functionality.
 * The camera shake alters the camera's Y position. Because of the aforementioned behaviour, the camera shake could actually cause the player to fall outside the camera's boundaries (above or below) and get killed. Again, within this project, the camera shake only happens after death, so it's not a problem, but might cause unexpected behavior if the project was to be continued.
 * There's a low chance that the game hangs when the player's colour is changed, because of the while loop. There's an insignificant chance that the randomizer would pick the same number over and over again, but I'm relying on the side of how mathemathically impossible it is for this to happen realistically.
+* The game's main design around actually moving upwards is not great, there's a chance the player will eventually reach a killscreen when some of the positions reach inf in size.
